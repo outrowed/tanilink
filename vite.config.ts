619bf749cdfff587074a4013e9e0fbb5fccccfcf
outrowed <outrowed@gmail.com>
2026-03-27 15:31:48 +0700

    fix: remove github.io base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,19 @@ import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-function getBasePath() {
-  const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1]
+// function getBasePath() {
+//   const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1]
 
-  if (process.env.GITHUB_ACTIONS !== "true" || !repositoryName) {
-    return "/"
-  }
+//   if (process.env.GITHUB_ACTIONS !== "true" || !repositoryName) {
+//     return "/"
+//   }
 
-  return repositoryName.endsWith(".github.io") ? "/" : `/${repositoryName}/`
-}
+//   return repositoryName.endsWith(".github.io") ? "/" : `/${repositoryName}/`
+// }
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: getBasePath(),
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
